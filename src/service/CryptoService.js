cbbae93757635027241e202ff2417bf94e904695
Thales Morato <tha.s.morato@gmail.com>
2022-02-15 11:53:35 -0300

    feat: crypto service list generator

--- src/service/CryptoService.js
+++ src/service/CryptoService.js
@@ -1,12 +1,23 @@
-import Crypto from '../entity/Crypto.js';
-import CryptoRepository from '../repository/CryptoRepository.js';
+import Crypto from "../entity/Crypto.js";
+import CryptoRepository from "../repository/CryptoRepository.js";
 
 class CryptoService {
   constructor({ repository } = {}) {
     this.repository = repository || new CryptoRepository();
   }
+
   async *list() {
     // TODO: implementar generator que chama a repository fazendo a paginação
+    let page = 0;
+
+    const dataIntoInstance = (data) => new Crypto(data);
+
+    while (true) {
+      page += 1;
+      const { data } = await this.repository.list(page);
+
+      yield data.map(dataIntoInstance);
+    }
   }
 }
 
