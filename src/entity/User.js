62ac39fd8dff7668b35001d70e9d9ba1158be8ef
Thales Morato <tha.s.morato@gmail.com>
2022-02-15 12:32:24 -0300

    feat: user to string

--- src/entity/User.js
+++ src/entity/User.js
@@ -3,6 +3,11 @@ class User {
     this.id = id;
   }
   // TODO: Alguma manipulação a fazer considerando que o objeto gerado será constantemente escrito em tela?
+
+  [Symbol.toPrimitive](coercionType) {
+    if (coercionType !== "string") throw new TypeError();
+    return `User-${this.id}`;
+  }
 }
 
 export default User;
